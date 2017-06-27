e44e9174ba56a54b93fcce87c284e99f37264967
kubk <0jwo4yfg79p@gmail.com>
2017-06-27 11:51:15 +0000

    Added function that emulates image downloading

---
+++ src/utils.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} base64
+ * @param {string} imageName
+ */
+export function downloadImage(base64, imageName) {
+    const link = document.createElement('a');
+    link.href = base64;
+    link.download = imageName;
+    // In Firefox we need explicitly add new link element to the DOM, otherwise click() will not work
+    document.body.appendChild(link);
+    link.click();
+}
